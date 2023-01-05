6bc95f8fca870046b60e65990c07cd05510485aa
Jim Barnett <hlbradow@yahoo.com>
2023-01-04 22:50:04 -0500

    first save

---
+++ mock_data.py
@@ -0,0 +1,58 @@
+catalog = [
+    {
+        title: "Brave New World",
+        price: 12.99,
+        category: "book",
+        image: "https://m.media-amazon.com/images/I/41-n-3hZMeL._SX325_BO1,204,203,200_.jpg",
+        author: "Aldous Huxley",
+    },
+    {
+        title: "Man and His Symbols",
+        price: 7.99,
+        category: "book",
+        image: "https://img.thriftbooks.com/api/images/m/f3c99bc9e96a34dfc7b0f7e0179a9eef9196dd3f.jpg",
+        author: "Carl Jung",
+    },
+    {
+        title: "Animal Farm",
+        price: 8.99,
+        category: "book",
+        image: "https://m.media-amazon.com/images/I/41lnnyUJzuL._SL350_.jpg",
+        author: "Goerge Orwell",
+    },
+    {
+        title: "1984",
+        price: 11.99,
+        category: "book",
+        image: "https://i0.wp.com/www.printmag.com/wp-content/uploads/2017/01/2a34d8_a6741e88335241308890543d203ad89dmv2.jpg?resize=500%2C815&ssl=1",
+        author: "George Orwell",
+    },
+    {
+        title: "7 Pillars of Wisdom",
+        price: 13.99,
+        category: "book",
+        image: "https://g.christianbook.com/dg/product/cbd/f400/821490.jpg",
+        author: "T.E. Lawrence",
+    },
+    {
+        title: "The Screwtape Letters",
+        price: 15.99,
+        category: "book",
+        image: "https://m.media-amazon.com/images/I/41NRIlfqwwL._SX330_BO1,204,203,200_.jpg",
+        author: "C.S. Lewis",
+    },
+    {
+        title: "The Doors of Perception",
+        price: 12.99,
+        category: "book",
+        image: "https://m.media-amazon.com/images/I/61QIZw3MOoL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+        author: "Aldous Huxley",
+    },
+    {
+        title: "The Pilgrim's Progress",
+        price: 17.99,
+        category: "book",
+        image: "https://m.media-amazon.com/images/I/51Vsn+EEFZL._SX381_BO1,204,203,200_.jpg",
+        author: "John Bunyan",
+    },
+]
